29057527cdbb8e44767a57fddefe1f1ef404f742
LeMyosotis <119458477+LeMyosotis@users.noreply.github.com>
2022-12-09 10:45:10 +0100

    Update button.js

--- script/button.js
+++ script/button.js
@@ -1,14 +1,14 @@
 const button = document.querySelector(".color-picker-button")
 const out = document.querySelector(".color-selector-out")
-const colorPicker = document.querySelector(".color-selector")
+const colorPickerContainer = document.querySelector(".color-selector")
 
 
 button.addEventListener("click", e => {
     button.classList.toggle("active");
-    colorPicker.classList.toggle("open");
+    colorPickerContainer.classList.toggle("open");
 });
 
 out.addEventListener("click", e => {
     button.classList.remove("active");
-    colorPicker.classList.remove("open");
+    colorPickerContainer.classList.remove("open");
 });
